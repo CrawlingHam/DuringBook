d9e3bd324bb5b556de8eaa502034b821bfacb72b
Farah.A <155859393+CrawlingHam@users.noreply.github.com>
2024-03-23 19:59:30 +0000

    Structuring in progress

---
+++ GameLibraries/MenuManagerLibrary/MenuManager.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+namespace MenuManagerLibrary;
+
+public class Menu
+{
+    Display newDisplay = new Display(); // Instantiate a new display
+    public void Run()
+    {
+        newDisplay.DisplayAbout(); // Display About logo
+        newDisplay.DisplayPrompt(); // Display prompt message
+    }
+}
---
+++ MainGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using GameManagerLibrary;
+
+namespace MainGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the game
+            Game newGame = new Game();
+            newGame.Start();
+        }
+    }
+}
